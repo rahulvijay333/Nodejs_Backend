d7b5b2c3a0d36b5c94ac0f753742b0aaeb380911
Rahul Vijay <djrahulv337@gmail.com>
2023-08-17 12:31:58 +0530

    profile image/certificate upload issue solved

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -19,51 +19,108 @@ const getProfile = async (req, res) => {
 }
 
 
-const updateDoctorProfile = async (req, res) => {
+// const updateDoctorProfile = async (req, res) => {
 
-  const { username, gender, speciality, phone, houseName, city, state, services, qualification } = req.body;
+//   const { username, gender, speciality, phone, houseName, city, state, services, qualification } = req.body;
+
+//   console.log(username);
+//   console.log(services);
+
+//   try {
+
+//     const doctor = await Doctor.findOne({ _id: req.userId });
+
+//     if (req.files) {
+
+//       const result = await cloudinary.uploader.upload(req.files.profilePic?.tempFilePath , { folder: 'Patients' });
+//       doctor.profilePicture.public_id = result.public_id;
+//       doctor.profilePicture.secure_url = result.secure_url
+
+//       const pdfResponse = await cloudinary.uploader.upload(req.files.certificate?.tempFilePath, { folder: 'Patients' });
+//       doctor.certificate.public_id = pdfResponse.public_id;
+//       doctor.certificate.secure_url = pdfResponse.secure_url;
+
+//     }
+
+//     doctor.fullName = username;
+//     doctor.gender = gender;
+//     doctor.phone = phone;
+//     doctor.services = services?.split(',');
+//     doctor.address = { houseName, city, state };
+//     doctor.speciality = speciality;
+//     doctor.qualification = qualification;
+
+
+//     await doctor.save()
+//     res.status(200).json({
+//       message: 'Updation success',
+//       user: doctor
+//     })
+
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).json({
+//       errorInfo: 'Internal server error'
+//     })
+//   }
+// }
+
+
+const updateDoctorProfile = async (req, res) => {
+  const {
+    username,
+    gender,
+    speciality,
+    phone,
+    houseName,
+    city,
+    state,
+    services,
+    qualification,
+  } = req.body;
 
   console.log(username);
-  console.log(services);
 
   try {
-
     const doctor = await Doctor.findOne({ _id: req.userId });
-
+    const profilePic = req.files?.profilePic?.tempFilePath;
+    const certificate = req.files?.certificate?.tempFilePath;
+    console.log(profilePic);
+    console.log(certificate);
     if (req.files) {
-
-      const result = await cloudinary.uploader.upload(req.files.profilePic?.tempFilePath , { folder: 'Patients' });
+      const result = await cloudinary.uploader.upload(profilePic, {
+        folder: "Patients",
+      });
       doctor.profilePicture.public_id = result.public_id;
-      doctor.profilePicture.secure_url = result.secure_url
+      doctor.profilePicture.secure_url = result.secure_url;
 
-      const pdfResponse = await cloudinary.uploader.upload(req.files.certificate?.tempFilePath, { folder: 'Patients' });
+      const pdfResponse = await cloudinary.uploader.upload(certificate, {
+        folder: "Patients",
+      });
       doctor.certificate.public_id = pdfResponse.public_id;
       doctor.certificate.secure_url = pdfResponse.secure_url;
-
     }
 
     doctor.fullName = username;
     doctor.gender = gender;
     doctor.phone = phone;
-    doctor.services = services?.split(',');
+    doctor.services = services?.split(",");
     doctor.address = { houseName, city, state };
     doctor.speciality = speciality;
     doctor.qualification = qualification;
 
-
-    await doctor.save()
+    await doctor.save();
     res.status(200).json({
-      message: 'Updation success',
-      user: doctor
-    })
-
+      message: "Updation success",
+      user: doctor,
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).json({
-      errorInfo: 'Internal server error'
-    })
+      errorInfo: "Internal server error",
+    });
   }
-}
+};
 
 const getSpecialities = async (req, res) => {
   try {
