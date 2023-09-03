98a03e5e67f6c5d7f95baf761e0b74cf9c44dc99
Rahul Vijay <djrahulv337@gmail.com>
2023-09-03 14:09:25 +0530

    notifications update

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -3,17 +3,17 @@ const Notification = require('../models/notification');
 const adminGetNotification = async (req, res) => {
     try {
 
-        const notification = await Notification.find({ recipient: req.userId , read: false });
+        const notification = await Notification.find({ recipient: req.userId, read: false });
 
         if (notification.length === 0) {
-            return res.status(400).json({ errorInfo: 'No Notifications' });
-        } 
+            return res.status(202).json({ errorInfo: 'No Notifications' });
+        }
 
         res.status(200).json({ notification });
 
     } catch (err) {
         res.status(200).json({
-            errorInfo:'Internal Server Error'
+            errorInfo: 'Internal Server Error'
         })
     }
 }
@@ -21,17 +21,17 @@ const adminGetNotification = async (req, res) => {
 const doctorGetNotification = async (req, res) => {
     try {
 
-        const notification = await Notification.find({ recipient: req.userId , read: false });
+        const notification = await Notification.find({ recipient: req.userId, read: false }).populate('sender', 'name').exec();;
 
         if (notification.length === 0) {
             return res.status(400).json({ errorInfo: 'No Notifications' });
-        } 
+        }
 
         res.status(200).json({ notification });
 
     } catch (err) {
         res.status(200).json({
-            errorInfo:'Internal Server Error'
+            errorInfo: 'Internal Server Error'
         })
     }
 }
@@ -39,39 +39,42 @@ const doctorGetNotification = async (req, res) => {
 const patientGetNotifcation = async (req, res) => {
     try {
 
-        const notification = await Notification.find({ recipient: req.userId  , read: false});
+        const notification = await Notification.find({ recipient: req.userId, read: false }).populate('sender', 'name').exec();
 
         if (notification.length === 0) {
-            return res.status(400).json({ errorInfo: 'No Notifications' });
-        } 
+            return res.status(202).json({
+
+                notification: []
+            });
+        }
 
         res.status(200).json({ notification });
 
     } catch (err) {
         res.status(200).json({
-            errorInfo:'Internal Server Error'
+            errorInfo: 'Internal Server Error'
         })
     }
 }
 
 const readNotification = async (req, res) => {
-  try {
-    // Find notifications to be marked as read
-    const notificationsToUpdate = await Notification.find({ recipient: req.userId , read: false });
+    try {
+        // Find notifications to be marked as read
+        const notificationsToUpdate = await Notification.find({ recipient: req.userId, read: false });
 
-    // Update the notifications to mark them as read
-    await Notification.updateMany({ recipient: req.userId , read: false }, { read: true });
+        // Update the notifications to mark them as read
+        await Notification.updateMany({ recipient: req.userId, read: false }, { read: true });
 
-    // Fetch the updated notifications
-      const notification = await Notification.find({ _id: req.userId  }).populate('sender')
-          ;
+        // Fetch the updated notifications
+        const notification = await Notification.find({ _id: req.userId }).populate('sender')
+            ;
 
-    res.status(200).json({ message: 'Notifications marked as read.', notification });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while marking notifications as read.' });
-  }
+        res.status(200).json({ message: 'Notifications marked as read.', notification });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while marking notifications as read.' });
+    }
 }
-    
+
 module.exports = {
     adminGetNotification,
     doctorGetNotification,
